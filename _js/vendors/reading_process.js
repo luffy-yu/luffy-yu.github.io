dba7a5d19c4b5481fb276b32e1a4850597c4563e
luffy-yu <yuliuchuan@gmail.com>
2020-11-12 00:39:15 +0800

    update progress bar

--- _js/vendors/reading_process.js
+++ _js/vendors/reading_process.js
@@ -11,9 +11,10 @@ $(document).ready(function () {
     } catch (e) {
     }
     window.addEventListener('scroll', function () {
+        var $landing = $('#landing')
         var $main = $('.main-block')
-        var h = $(window).scrollTop() - $main.position().top
-        var total = $main.height() - document.documentElement.clientHeight
+        var h = $landing.height() + $(window).scrollTop() - $main.position().top
+        var total = document.documentElement.scrollHeight - document.documentElement.clientHeight
         var percent = Math.round(h / total * 100)
         if (percent < 0) percent = 0
         if (percent > 100) percent = 100
